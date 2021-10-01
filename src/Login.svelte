1aafc90b581b04bde30d4cdd74b077de5c5a6e9b
Flavia <flavia@miyagami.comm>
2021-10-01 14:49:49 +0200

    Sign in page

--- src/Login.svelte
+++ src/Login.svelte
@@ -20,67 +20,47 @@
 
 </script>
 
-<!--<label for="username">Username</label>-->
-<!--<input name="username" bind:value={username} minlength="30" maxlength="16" />-->
-
-<!--<label for="password">Password</label>-->
-<!--<input name="password" bind:value={password}  type="password" />-->
-
-<!--<button class="rounded-md bg-black" on:click={login}>Login</button>-->
-<!--<button class="rounded-md" on:click={singnup}>Sign UP</button>-->
 
 <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-red-600.svg" alt="Workflow">
+        <img class="mx-auto h-16 w-auto" src="miyagami-logo.png" alt="Workflow">
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
         </h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
-            Or
-            <a href="#" class="font-medium text-red-600 hover:text-red-500">
-                start your 14-day free trial
-            </a>
-        </p>
     </div>
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <form class="space-y-6" action="#" method="POST">
                 <div>
-                    <label for="email" class="block text-sm font-medium text-gray-700">
-                        Email address
+                    <label for="username" class="block text-sm font-medium text-gray-700">
+                       Username
                     </label>
                     <div class="mt-1">
-                        <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm">
+                        <input  bind:value={username} id="username" name="username" type="text" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm">
                     </div>
                 </div>
 
                 <div>
-                    <label for="password" class="block text-sm font-medium text-gray-700">
+                    <label  for="password" class="block text-sm font-medium text-gray-700">
                         Password
                     </label>
                     <div class="mt-1">
-                        <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm">
+                        <input bind:value={password} id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm">
                     </div>
                 </div>
 
                 <div class="flex items-center justify-between">
-                    <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-red-600 focus:ring-red-500 border-gray-300 rounded">
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">
-                            Remember me
-                        </label>
-                    </div>
-
-                    <div class="text-sm">
-                        <a href="#" class="font-medium text-red-600 hover:text-red-500">
-                            Forgot your password?
-                        </a>
-                    </div>
+                   
                 </div>
 
                 <div>
-                    <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                    <button   on:click={login} type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                        Login
+                    </button>
+                </div>
+                <div>
+                    <button  on:click={singnup} type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
                         Sign in
                     </button>
                 </div>
@@ -93,7 +73,7 @@
                     </div>
                     <div class="relative flex justify-center text-sm">
             <span class="px-2 bg-white text-gray-500">
-              Or continue with
+              Follow us
             </span>
                     </div>
                 </div>
@@ -101,7 +81,7 @@
                 <div class="mt-6 grid grid-cols-3 gap-3">
                     <div>
                         <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-                            <span class="sr-only">Sign in with Facebook</span>
+                            <span class="sr-only">Linkedin</span>
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z" clip-rule="evenodd" />
                             </svg>
