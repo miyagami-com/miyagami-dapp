f9c65255a764240390737b8024bd3361e83f8cb9
nikitaSazhinov <nikitasazhinovpro@gmail.com>
2021-10-01 16:47:12 +0200

    encrypted messages

--- src/Chat.svelte
+++ src/Chat.svelte
@@ -1,51 +1,53 @@
 <script>
-    import { username,user } from './user'
- //   import {messages} from './store.js'
+    import { username } from './user'
     import GUN from 'gun';
     import 'gun/sea';
     import 'gun/axe';
-    import {writable} from 'svelte/store'
     import { onMount } from 'svelte';
 
-    let message = '';
+    let newMessage = '';
     let messages = []
     const db = GUN();
-    
+    let key = "thisisthekey"
+
     // Define db
     onMount(() => {
+        db.get('chatMiyagami').map().once( async (data) => {        
 
-        db.get('chatmiyagami').map().once( async (v) => {
-        console.log(v)
-        console.log("added")
-        // $messages = [...$messages, v]
-        // console.log($messages);
-        messages = [...messages.slice(-100), v]
-        console.log(messages);
+        var message = {
+            text: (await SEA.decrypt(data.text, key)) + '',
+            user: $username,
+            time: GUN.state.is(data, 'text')
+        }
+        messages = [...messages.slice(-5), message]
+
         });
-
+        
     })
-    // get messages (limit?)
 
     // function to create/send message
-    function submitMessage() {
-        const itemname = new Date().toISOString();
-        db.get('chatmiyagami').get(itemname).put({
-            text: message,
+    async function submitMessage() {
+
+        const secret = await SEA.encrypt(newMessage, key);
+        console.log({secret});
+        const message = db.get("chatMiyagami").set({
+            text: secret,
             user: $username,
             time: new Date().toISOString()
-        })
+            });
+        const itemname = new Date().toISOString();
+        db.get('chatMiyagami').get(itemname).put(message)
+
     }
         
-        // encrypt messages
+    // encrypt messages
 
     // function to get more messages
 
 </script>
 
-<label>Username</label>
-<label>Username</label>
 <form on:submit|preventDefault={submitMessage}>
-    <input bind:value={message}/>
+    <input bind:value={newMessage}/>
     <button type="submit">say</button>
 </form>
 {#each messages as message}
