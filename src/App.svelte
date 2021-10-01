e3d66020da2b9e1661d30bf0347d006e7abd43fb
dale wesdorp <dalewesdorp@gmail.com>
2021-10-01 14:13:43 +0200

    Added tailwind css

--- src/App.svelte
+++ src/App.svelte
@@ -6,29 +6,13 @@
 </script>
 
 <main>
-	
+
 	<Header />
 	<Login />
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
