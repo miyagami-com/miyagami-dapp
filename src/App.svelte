ffb264e34d0c786bc9d0530da87fbe049a79a995
dale wesdorp <dalewesdorp@gmail.com>
2021-10-01 14:30:21 +0200

    Init tailwind CSS

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
 	<Message />
 
 	<Header />
-<!--	<Login />-->
+	<Login />
 </main>
 
 <style global lang="postcss">
--- src/Header.svelte
+++ src/Header.svelte
@@ -32,11 +32,11 @@ function signout() {
             <div class="flex justify-start lg:w-0 lg:flex-1">
                 <a href="#">
                     <span class="sr-only">Workflow</span>
-                    <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
+                    <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-red-600.svg" alt="">
                 </a>
             </div>
             <div class="-mr-2 -my-2 md:hidden">
-                <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+                <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500" aria-expanded="false">
                     <span class="sr -only">Login</span>
                 </button>
             </div>
@@ -44,7 +44,7 @@ function signout() {
                 <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                     Sign in
                 </a>
-                <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700">
                     Sign up
                 </a>
             </div>
